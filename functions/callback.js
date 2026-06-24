e538fdca1d2f5e6739f9d1a0701c6be77ea680af
Frankie1000000 <163623764+Frankie1000000@users.noreply.github.com>
2026-06-24 13:07:32 +0300

    Update callback.js

--- functions/callback.js
+++ functions/callback.js
@@ -1,41 +1,59 @@
-const fetch = require("node-fetch");
-
 exports.handler = async (event) => {
   const code = event.queryStringParameters.code;
-  const clientId = process.env.OAUTH_CLIENT_ID;
+  const clientId = "Ov23liH4zHIasLP20pCs";
   const clientSecret = process.env.OAUTH_CLIENT_SECRET;
 
-  const response = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({ 
-      client_id: clientId, 
-      client_secret: clientSecret, 
-      code 
-    })
-  });
+  try {
+    const response = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code
+      })
+    });
 
-  const data = await response.json();
+    const data = await response.json();
+    const token = data.access_token;
 
-  const script = `
-    <script>
-      const token = "${data.access_token}";
-      const message = token 
-        ? { token, provider: "github" }
-        : { error: "OAuth failed" };
-      window.opener.postMessage(
-        "authorization:github:success:" + JSON.stringify(message),
-        "*"
-      );
-    </script>
-  `;
+    const html = `
+      <!DOCTYPE html>
+      <html>
+      <body>
+      <script>
+        (function() {
+          function receiveMessage(e) {
+            console.log("receiveMessage %o", e);
+          }
+          window.addEventListener("message", receiveMessage, false);
+          
+          const token = ${JSON.stringify(token)};
+          const mess = token
+            ? "authorization:github:success:" + JSON.stringify({ token: token, provider: "github" })
+            : "authorization:github:error:token not found";
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: script
-  };
+          window.opener.postMessage(mess, "*");
+        })();
+      </script>
+      <p>Authorizing... you can close this window.</p>
+      </body>
+      </html>
+    `;
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: html
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 };
