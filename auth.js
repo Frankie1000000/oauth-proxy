5e4bab4d5d7a0ece7fba215992d3401ceb714630
Frankie1000000 <163623764+Frankie1000000@users.noreply.github.com>
2026-06-24 12:22:18 +0300

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  const clientId = process.env.OAUTH_CLIENT_ID;
-  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo`;
-  
-  return {
-    statusCode: 302,
-    headers: { Location: redirectUrl }
-  };
-};
